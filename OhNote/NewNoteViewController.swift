e68190f596e9fb7f1ee01f64cac5986646da94ab
吴伟城 <wuvcen@gmail.com>
2016-02-16 21:26:14 +0800

    Framework-ZSSRichTextEditor added again.

---
+++ OhNote/NewNoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewNoteViewController.swift
+//  OhNote
+//
+//  Created by 吴伟城 on 16/2/16.
+//  Copyright © 2016年 Weicheng Wu. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: ZSSRichTextEditor {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
